033bed8d7d37c09c40b8a8bde56141c8509985df
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-07-01 19:30:07 +0200

    Problem 4

---
+++ coding_challenges/problem_four.py
@@ -0,0 +1,14 @@
+# List of integers
+TheList = [5, 6, 9, 24, 10, 15, 17, 18, 21]
+
+# Variable that holds the number of numbers divisible by 3
+yes = 0
+
+# Looping through the list of integers
+for i in range(0, len(TheList)):
+    # Checking if the number is divisible by 3
+    if TheList[i] % 3 == 0:
+        yes += 1
+        
+# Displaying result
+print(yes, "of elements are multiples of 3")
