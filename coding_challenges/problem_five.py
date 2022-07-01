8c89cd286328eab4216c9ec5081ebaa0513f8694
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-07-01 19:46:16 +0200

    Commented it out

--- coding_challenges/problem_five.py
+++ coding_challenges/problem_five.py
@@ -2,11 +2,15 @@
 # List of integers
 TheList = [5, 6, 9, 24, 10, 15, 17, 18, 21, 22, 24]
 
+# Sorting array in descending order
 arr = sorted(TheList, reverse=True)
 
-for i in range(1, len(TheList)):
+# Looping through the list of integers
+for i in range(len(TheList)):
+    # Check if two numbers are equal and skip
     if arr[i] == arr[0]:
         continue
+    # If the number is not equal to the current value, print it out
     else:
         print(arr[i])
         break
