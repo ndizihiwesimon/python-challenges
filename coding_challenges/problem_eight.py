2a837fbb16075407542337b2a7c0e4a659c89e26
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-07-02 00:37:27 +0200

    Eight and six

--- coding_challenges/problem_eight.py
+++ coding_challenges/problem_eight.py
@@ -1,1 +1,2 @@
-# *Problem 8*. Given a list L = [c1 , ..., cn ] of n distinct characters, write a program which generates a random string s such that every character of s is an element of L and such that character ci occurs i times in s.
+# *Problem 8*. Given a list L = [c1 , ..., cn ] of n distinct characters, 
+# write a program which generates a random string s such that every character of s is an element of L and such that character ci occurs i times in s.
--- coding_challenges/problem_six.py
+++ coding_challenges/problem_six.py
@@ -2,16 +2,13 @@
 # *Problem 6*. You are given an un-ordered array consisting of integers without any duplicates.
 # You are allowed to swap any two elements. You need to find the minimum number of swaps required to sort the array in ascending order
 
-from os import times
-
-
 arr = [2,33,92,100]
 
-times = 0
+timez = 0
 
 for i in range(len(arr)) :
     for j in range(len(arr)):
         if arr[i] > arr[j]:
-            times += 1
+            timez += 1
     
-print(times)
+print(timez)
