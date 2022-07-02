bccf120e709bdacca5f97ea64fc3d69dfaaefc4d
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-07-02 15:56:26 +0200

    Eight

--- coding_challenges/problem_eight.py
+++ coding_challenges/problem_eight.py
@@ -1,9 +1,24 @@
 # *Problem 8*. Given a list L = [c1 , ..., cn ] of n distinct characters, 
 # write a program which generates a random string s such that every character of s is an element of L and such that character ci occurs i times in s.
 
-n = int(input("Enter value of n: "))
+import random
 
-L = [n]
+def randomString(n):
+    # Given a List of of distinct characters
+    L = ['a', 'b', 'c', 'd', 'e', 'f', 'g',
+        'h', 'i', 'j', 'k', 'l', 'm', 'n',
+        'o', 'p', 'q', 'r', 's', 't', 'u',
+        'v', 'w', 'x', 'y', 'z']
+    
+    # Variable to store generated random string
+    s = ""
 
-for i in range(1, n):
-    input(L[i])
+    # Generate a random string
+    for i in range(n):
+        s = s + L[random.randint(0,len(L)-1)]
+    
+    return s
+
+# Displaying random string
+print(randomString(5))
+
