40bce208cece874accb9016df3f3de87e81dfb94
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-07-02 15:25:59 +0200

    seven

--- coding_challenges/problem_seven.py
+++ coding_challenges/problem_seven.py
@@ -1,1 +1,14 @@
 # Write a function which randomly shuffles a list.
+
+import random
+
+# list of 0 to 5 
+l = list(range(5))
+
+# before shuffle
+print(l)
+
+
+random.shuffle(l)
+# After shuffle
+print(l)
