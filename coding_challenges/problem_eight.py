be8782dca6781e36be4a2c571b93599637b5b461
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-07-02 15:03:24 +0200

    Question six

--- coding_challenges/problem_eight.py
+++ coding_challenges/problem_eight.py
@@ -1,2 +1,9 @@
 # *Problem 8*. Given a list L = [c1 , ..., cn ] of n distinct characters, 
 # write a program which generates a random string s such that every character of s is an element of L and such that character ci occurs i times in s.
+
+n = int(input("Enter value of n: "))
+
+L = [n]
+
+for i in range(1, n):
+    input(L[i])
--- coding_challenges/problem_six.py
+++ coding_challenges/problem_six.py
@@ -1,14 +1,48 @@
 
 # *Problem 6*. You are given an un-ordered array consisting of integers without any duplicates.
 # You are allowed to swap any two elements. You need to find the minimum number of swaps required to sort the array in ascending order
+ 
 
-arr = [2,33,92,100]
+# Return the minimum number of swaps required to sort the array
+def minSwap(arr, n):
+	
+	ans = 0
+	temp = arr.copy()
 
-timez = 0
+	# Dictionary which stores the
+	# indexes of the input array
+	h = {}
 
-for i in range(len(arr)) :
-    for j in range(len(arr)):
-        if arr[i] > arr[j]:
-            timez += 1
-    
-print(timez)
+	temp.sort()
+
+	for i in range(n):
+		h[arr[i]] = i
+		
+	init = 0
+	
+	for i in range(n):
+
+		# This is checking whether
+		# the current element is
+		# at the right place or not
+		if (arr[i] != temp[i]):
+			ans += 1
+			init = arr[i]
+
+			# If not, swap this element
+			# with the index of the
+			# element which should come here
+			arr[i], arr[h[temp[i]]] = arr[h[temp[i]]], arr[i]
+
+			# Update the indexes in
+			# the hashmap accordingly
+			h[init] = h[temp[i]]
+			h[temp[i]] = i
+	return ans
+
+a = [ 10, 2, 3, 7]
+n = len(a)
+
+# Output will be 3
+print(minSwap(a, n))
+
