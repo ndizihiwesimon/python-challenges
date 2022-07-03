4b515cc57b9aa3f555303e846abd52fb15fd3be9
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-07-03 14:48:59 +0200

    twenty

--- coding_challenges/problem_twenty.py
+++ coding_challenges/problem_twenty.py
@@ -1,5 +1,13 @@
-# *Problem 20*: Each year the School of Computing Science needs to process exam marks for the students in the department. The highest achieving student gets a price and meets the Head of School. The people who fail a course receive a letter with information about what courses they’ve failed. The School needs to report how the class did on average. To progress to Honours students need to pass all courses and get an average of 70. How many satisfy the requirement? Finally,
-# each year the Head of School gives a raise to the lecturer of the best performing course. It’s also of interest to know which subject has worst performance. The staff are too busy, so they’ve asked you to write a program that can help them get that information. They’ve helpfully processed it for you, so that all you need is stored in lists.
+# *Problem 20*: Each year the School of Computing Science needs to process exam marks for the students in the department. 
+# The highest achieving student gets a price and meets the Head of School. The people who fail a course receive a letter with information about what courses they’ve failed. 
+# The School needs to report how the class did on average. 
+# To progress to Honours students need to pass all courses and get an average of 70. 
+# How many satisfy the requirement? Finally,
+# each year the Head of School gives a raise to the lecturer of the best performing course. 
+# It’s also of interest to know which subject has worst performance. 
+# The staff are too busy, so they’ve asked you to write a program that can help them get that information. 
+# They’ve helpfully processed it for you, so that all you need is stored in lists.
+# 
 # ``marks = [[56,89,70,92,67,100],[60,70,100,45,70,76],
 # [60,95,90,85,93,45],[55,80,56,45,51,76],[78,100,65,77,91,87],
 # [45,78,65,50,45,87],[32,50,45,67,40,80]]
