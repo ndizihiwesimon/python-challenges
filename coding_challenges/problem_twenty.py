1915712ef27696957e0ec7bf191c2e35d5b02a09
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-07-03 14:51:07 +0200

    twenty comments

--- coding_challenges/problem_twenty.py
+++ coding_challenges/problem_twenty.py
@@ -11,9 +11,9 @@
 # ``marks = [[56,89,70,92,67,100],[60,70,100,45,70,76],
 # [60,95,90,85,93,45],[55,80,56,45,51,76],[78,100,65,77,91,87],
 # [45,78,65,50,45,87],[32,50,45,67,40,80]]
+# 
 # students = ["Pelin","Dominique","Valentin","Christine","John Paul","
 # Patrick","Kellen"]
-# subjects = ["Algorighms and Data Structures","Java","Web App
-# Development","Databases", "Human Computer Interaction","
-# Information Retrieval"]``
+# 
+# subjects = ["Algorithms and Data Structures","Java","Web App Development","Databases", "Human Computer Interaction", "Information Retrieval"]``
 
