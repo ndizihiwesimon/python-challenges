55b50efca68aab818ab736d10d5e916ff513a717
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-07-03 09:13:28 +0200

    Eighteen

---
+++ coding_challenges/problem_eighteen.py
@@ -0,0 +1,5 @@
+# *Problem 18*. Write a function of your choosing which takes and returns a number (such as def double(x): return x*2). 
+# Write another function which checks to see whether the input has been seen before. If it has not, 
+# use the function to calculate the  result and save the calculation in a dictionary, where the key is the input and the value is the result. 
+# If it has been seen already, look up the result in the dictionary. For the double function given as an example, 
+# you should be able to build the relevant dictionary from the reading problems at the beginning of this section.
