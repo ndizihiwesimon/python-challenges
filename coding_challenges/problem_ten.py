5f4db0752720731478029eca791a3cbca5c48e61
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-07-04 22:10:05 +0200

    ten

--- coding_challenges/problem_ten.py
+++ coding_challenges/problem_ten.py
@@ -1,7 +1,7 @@
 # *Problem 10*. Write a function that takes two numbers and returns whether the second is a factor of the first.
 
 def isFactorOf(a, b):
-    if a % b == 0:
+    if a % b == 0: # Condition to check if the second is a factor of the first
         return print(a, "Is a factor of", b)
     else:
         return print(a, "Is not a factor of", b)
